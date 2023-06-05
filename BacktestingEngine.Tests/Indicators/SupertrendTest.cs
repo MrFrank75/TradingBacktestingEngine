84cf7a6d2f46e5145fa5c3afefb7550b5777c8a1
Livio Francescucci <livio.francescucci@gmail.com>
2023-06-05 18:08:04 +0200

    Fixed BTC Supertrend test

--- BacktestingEngine.Tests/Indicators/SupertrendTest.cs
+++ BacktestingEngine.Tests/Indicators/SupertrendTest.cs
@@ -10,9 +10,32 @@ namespace BacktestingEngine.Test.Indicators
         {
         }
 
+
+        [Test]
+        [TestCase(10,48204.71)]
+        [TestCase(11,48199.21)]
+        [TestCase(12,48030.46)]
+        public void SupertrendBTC_ReturnsExpectedValues(int numOfItems, decimal expectedSuperTrend)
+        {
+            var sut = new BacktestingEngine.Indicators.Supertrend(10, 3);
+
+            var pricesReader = new PricesReader();
+            var priceCandlesticks = pricesReader.ReadPricesVector("BINANCE", "BTCUSDT", "60").ToList();
+            var initialDataSet = priceCandlesticks.Take(numOfItems).ToList();
+
+            SupertrendResult result = new SupertrendResult();
+            foreach (var candlestick in initialDataSet)
+            {
+                result = sut.CalculateSuperTrend(candlestick);
+            }
+
+
+            Assert.That(result.Value, Is.EqualTo(expectedSuperTrend).Within(0.1m));
+        }
+
         [Test]
         [TestCase(10, 0.0083)]
-        public void Supertrend_ReturnsExpectedValues(int numOfItems, decimal expectedSuperTrend)
+        public void SupertrendMATIC_ReturnsExpectedValues(int numOfItems, decimal expectedSuperTrend)
         {
             var sut = new BacktestingEngine.Indicators.Supertrend(10, 3);
 
--- BacktestingEngine/Indicators/Supertrend.cs
+++ BacktestingEngine/Indicators/Supertrend.cs
@@ -4,6 +4,7 @@ using BacktestingEngine.Indicators;
 namespace BacktestingEngine.Indicators
 {
     public class Supertrend {
+        private readonly int period;
         private readonly decimal multiplier;
         private decimal[] upperBand = new decimal[2];
         private decimal[] lowerBand = new decimal[2];
@@ -16,12 +17,13 @@ namespace BacktestingEngine.Indicators
         private List<Candlestick> prices;
 
         private Trend previousTrend = Trend.None;
-        private Candlestick previousCandle = new Candlestick { Close = decimal.MinValue };
+        private Candlestick previousCandle = new Candlestick { Close = 0};
 
         public Supertrend(int period, decimal multiplier)
         {
             this.atrCalculator = new ATR(period);
             this.prices = new List<Candlestick>();
+            this.period = period;
             this.multiplier = multiplier;
         }
 
@@ -29,42 +31,48 @@ namespace BacktestingEngine.Indicators
         {
             prices.Add(candle);
             var  currentTrend = previousTrend;
+            var trendHasChangedToDown = false;
+            var trendHasChangedToUp = false;
+
             decimal hl2 = (candle.High + candle.Low) / 2;
 
             // Calculate the Average True Range (ATR)
             atrValues[CURRENT] = Math.Round(atrCalculator.CalculateAverageTrueRange(prices, SmoothingType.RMA), 2);
             var atr = atrValues[CURRENT];
 
-            upperBand[CURRENT] = hl2 + this.multiplier * atr;
-            lowerBand[CURRENT] = hl2 - this.multiplier * atr;
-
-            lowerBand[CURRENT] = lowerBand[CURRENT] > lowerBand[PREVIOUS] || previousCandle.Close < lowerBand[PREVIOUS] ? lowerBand[CURRENT] : lowerBand[PREVIOUS];
-            upperBand[CURRENT] = upperBand[CURRENT] < upperBand[PREVIOUS] || previousCandle.Close > upperBand[PREVIOUS] ? upperBand[CURRENT] : upperBand[PREVIOUS];
-
-            var prevSuperTrend = superTrend[PREVIOUS];
-            if (atrValues[PREVIOUS] == 0)
+            if (prices.Count >= period)
             {
-                currentTrend = Trend.Down;
-            }
-            else if (prevSuperTrend == upperBand[PREVIOUS])
-            {
-                currentTrend = candle.Close > upperBand[CURRENT] ? Trend.Up : Trend.Down;
-            }
-            else
-                currentTrend = candle.Close < lowerBand[CURRENT] ? Trend.Down : Trend.Up;
+                upperBand[CURRENT] = hl2 + this.multiplier * atr;
+                lowerBand[CURRENT] = hl2 - this.multiplier * atr;
 
-            superTrend[CURRENT] = currentTrend == Trend.Up ? lowerBand[CURRENT] : upperBand[CURRENT];
+                lowerBand[CURRENT] = lowerBand[CURRENT] > lowerBand[PREVIOUS] || previousCandle.Close < lowerBand[PREVIOUS] ? lowerBand[CURRENT] : lowerBand[PREVIOUS];
+                upperBand[CURRENT] = upperBand[CURRENT] < upperBand[PREVIOUS] || previousCandle.Close > upperBand[PREVIOUS] ? upperBand[CURRENT] : upperBand[PREVIOUS];
 
-            var trendHasChangedToDown = previousTrend == Trend.Up && candle.Close < superTrend[CURRENT];
-            if (trendHasChangedToDown)
-            {
-                currentTrend = Trend.Down;
-            }
+                var prevSuperTrend = superTrend[PREVIOUS];
+                if (atrValues[PREVIOUS] == 0)
+                {
+                    currentTrend = Trend.Down;
+                }
+                else if (prevSuperTrend == upperBand[PREVIOUS])
+                {
+                    currentTrend = candle.Close > upperBand[CURRENT] ? Trend.Up : Trend.Down;
+                }
+                else
+                    currentTrend = candle.Close < lowerBand[CURRENT] ? Trend.Down : Trend.Up;
 
-            var trendHasChangedToUp = previousTrend == Trend.Down && candle.Close > superTrend[CURRENT];
-            if (trendHasChangedToUp)
-            {
-                currentTrend = Trend.Up;
+                superTrend[CURRENT] = currentTrend == Trend.Up ? lowerBand[CURRENT] : upperBand[CURRENT];
+
+                trendHasChangedToDown = previousTrend == Trend.Up && candle.Close < superTrend[CURRENT];
+                if (trendHasChangedToDown)
+                {
+                    currentTrend = Trend.Down;
+                }
+
+                trendHasChangedToUp = previousTrend == Trend.Down && candle.Close > superTrend[CURRENT];
+                if (trendHasChangedToUp)
+                {
+                    currentTrend = Trend.Up;
+                }
             }
 
             //prepare everything for the next iteration
